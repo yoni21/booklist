50796b686c9425974f6280afcb4b7436799e2303
yonas samuel <yonastlolo@yahoo.com>
2017-02-11 00:11:08 -0500

    add csv & json response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,26 @@
 class BooksController < ApplicationController
+  def index
+   @books = Book.all
+
+   respond_to do |format|
+     format.html  #http://localhost:3000/books.html
+     format.text
+     format.csv do
+       formated_csv = Book.generate_csv(@books)
+       render plain: formated_csv
+     end
+     format.json do
+       render json: @books.map { |book|
+         { title: book.title,
+           author: book.author,
+           already_read: book.already_read
+         }
+       }
+     end
+
+   end
+
+ end
 
 
 end
