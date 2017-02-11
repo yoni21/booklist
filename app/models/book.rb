e2e96c91f51e395326fd258ad5dadb1848bb8803
yonas samuel <yonastlolo@yahoo.com>
2017-02-11 00:22:40 -0500

    add csv response

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,7 @@
 require 'csv'
 
 class Book < ActiveRecord::Base
-  # This method is used to create a CSV representation of the data in the database
-  def self.generate_csv(book_list)
+    def self.generate_csv(book_list)
     header = ['id', 'title', 'author', 'already_read']
 
     CSV.generate(headers: true) do |csv|
